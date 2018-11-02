3bdc453cd0e75fe939f709f0486126bff391df9b
davidkolundzic <david_k42@hotmail.com>
2018-11-02 16:30:34 +0100

    medju commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 // import 'semantic-ui-css/semantic.min.css';
 import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+, document.getElementById('root'));
 
 
 
