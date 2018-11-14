4fb8871cfcd82775d1d7c085ec38c9554181c0dc
davidkolundzic <david_k42@hotmail.com>
2018-11-14 14:56:29 +0100

    test google search places

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import { connect  } from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Button } from "semantic-ui-react";
 import { incrementCounter, decrementCounter } from "./testActions";
+import Script from "react-load-script";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 
-const mapState = (state) => ({
-    data: state.test.data    
-})
-const actions={
-    incrementCounter,
-    decrementCounter
-}
+const actions = {
+  incrementCounter,
+  decrementCounter
+};
 
 class TestComponent extends Component {
-    render() {
-        const{decrementCounter, incrementCounter, data} = this.props;
-        return (
-            <div>
-                <h1>Test Area</h1>
-                <h3>The answer is: {data}</h3>
-                <Button onClick={incrementCounter} color='green' content='Increment'/>
-                <Button onClick={decrementCounter} color='red' content='Decrement'/>
-            </div>
-        );
-    }
+  state = {
+    address: "",
+    scriptsLoaded: false
+  };
+  handleScriptLoad = () => {
+    this.setState({ scriptsLoaded: true });
+  };
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
+  onChange = address => this.setState({ address });
+
+  render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
+    return (
+      <div>
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDYjhvBnLBL-NbvYbKcLbGAZZ7dhXDqBrg&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
+        <br />
+        <br />
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptsLoaded && (
+            <PlacesAutocomplete inputProps={inputProps} />
+          )}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default connect(mapState, actions) (TestComponent);
+export default TestComponent;
